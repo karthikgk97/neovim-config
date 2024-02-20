d9babed9964c310e22e6f211984d22c8f7516351
Karthik Ganesan <karthik.gk97@gmail.com>
2024-02-19 23:00:54 -0600

    added comments

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -10,6 +10,8 @@ return {
     config = function()
       require("mason-lspconfig").setup({
         ensure_installed = { "lua_ls", "pyright", "ruff_lsp" }
+      -- navigate to this website for most lsp languages list
+        -- https://github.com/williamboman/mason-lspconfig.nvim
       })
     end
   },
