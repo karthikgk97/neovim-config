b4d649607492af68d999ca39d1d01eb07035bf88
Karthik Ganesan <karthik.gk97@gmail.com>
2024-02-19 22:58:41 -0600

    added ep3 stuff
    added lsp support for python and lua. added mason lsp config and nvim lspconfig

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -8,6 +8,6 @@ return {
       },
         config = function()
           vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>', {})
-          vim.keymap.set('n', '<leader>n', ':Neotree toggle', {})
+          vim.keymap.set('n', '<leader>n', ':Neotree toggle<CR>', {})
         end
 }
--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -2,4 +2,6 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=4")
 vim.cmd("set number")
 vim.cmd("set wrap")
+vim.cmd("set softtabstop=4")
+vim.cmd("set shiftwidth=2")
 vim.g.mapleader = " "
