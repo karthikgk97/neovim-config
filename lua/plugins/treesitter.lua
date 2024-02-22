6136a41294ace1882ec28fdf157a332e48c2cec1
Karthik Ganesan <karthik.gk97@gmail.com>
2024-02-21 22:16:13 -0600

    added none-ls
    still have no idea what this is for

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,9 +4,9 @@ return {
         config = function()
           local config = require("nvim-treesitter.configs")
           config.setup({
-                ensure_installed = {"lua", "python", "rust"},
-                highlight = { enable = true },
-                indent = { enable = true }
+            auto_install = true,    
+            highlight = { enable = true },
+            indent = { enable = true }
           })
         end
 }
