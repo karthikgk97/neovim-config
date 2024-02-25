9cc737c49f939ae299a31b740a3f03c8a10e1335
Karthik Ganesan <karthik.gk97@gmail.com>
2024-02-24 19:10:05 -0600

    added relative number and python comment
    added shortcuts for python comment and uncomment

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -1,8 +1,15 @@
 vim.cmd("set expandtab")
 vim.cmd("set tabstop=4")
 vim.cmd("set number")
+vim.cmd("set relativenumber")
 vim.cmd("set wrap")
 vim.cmd("set softtabstop=4")
 vim.cmd("set shiftwidth=2")
 vim.cmd("set pumheight=10")
+vim.cmd("set nohlsearch")
 vim.g.mapleader = " "
+
+-- shortcut for commenting out in python files
+vim.keymap.set('x', '<leader>cp', ":s/^/#<CR>", {noremap = true})
+vim.keymap.set('x', '<leader>ucp', ":s/#/<CR>", {noremap = true})
+
