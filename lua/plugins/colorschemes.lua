09ca4fcad89356af8f2764078b41129c115485fb
Karthik Ganesan <karthik.gk97@gmail.com>
2024-02-24 18:12:21 -0600

    added colorschemes

---
+++ lua/plugins/colorschemes.lua
@@ -0,0 +1,33 @@
+return	{
+  {
+    "catppuccin/nvim",
+    lazy=false,
+    name = "catppuccin",
+    priority = 1000,
+    config = function()
+      -- vim.cmd.colorscheme "catppuccin"
+    end
+  },
+  {
+    "rebelot/kanagawa.nvim",
+    name = "kanagawa",
+    priority = 1000,
+    config = function()
+      local kanagawaConfig = require('kanagawa')
+      -- sample comment
+      kanagawaConfig.setup({
+        commentStyle = { italic = true },
+        functionStyle = { italic = false }
+      })
+      vim.cmd.colorscheme "kanagawa-dragon"
+    end
+  },
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      -- vim.cmd.colorscheme "tokyonight-night"
+    end
+  }
+}
