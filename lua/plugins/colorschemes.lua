4f2983f67d1da0adbc41ae4123fa5dad85528553
Karthik Ganesan <karthik.gk97@gmail.com>
2024-03-06 21:56:47 -0600

    modified catpuccin theme

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -1,26 +1,36 @@
-return	{
+return {
   {
     "catppuccin/nvim",
-    lazy=false,
+    lazy = false,
     name = "catppuccin",
     priority = 1000,
     config = function()
-      -- vim.cmd.colorscheme "catppuccin"
-    end
+      local catpuccinConfig = require("catppuccin")
+      catpuccinConfig.setup({
+        color_overrides = {
+          mocha = {
+            base = "#000000",
+            mantle = "#000000",
+            crust = "#000000",
+          },
+        },
+      })
+      vim.cmd.colorscheme("catppuccin")
+    end,
   },
   {
     "rebelot/kanagawa.nvim",
     name = "kanagawa",
     priority = 1000,
     config = function()
-      local kanagawaConfig = require('kanagawa')
+      local kanagawaConfig = require("kanagawa")
       -- sample comment
       kanagawaConfig.setup({
         commentStyle = { italic = true },
-        functionStyle = { italic = false }
+        functionStyle = { italic = false },
       })
-      vim.cmd.colorscheme "kanagawa-dragon"
-    end
+      -- vim.cmd.colorscheme "kanagawa-dragon"
+    end,
   },
   {
     "folke/tokyonight.nvim",
@@ -28,12 +38,12 @@ return	{
     priority = 1000,
     config = function()
       -- vim.cmd.colorscheme "tokyonight-night"
-    end
+    end,
   },
   {
     "ellisonleao/gruvbox.nvim",
     config = function()
       -- vim.cmd.colorscheme "gruvbox"
-    end
-  }
+    end,
+  },
 }
